6e840dd310abf9a887796c13a373d1a8810da8f5
xhelg <redice.guard@gmail.com>
2023-01-17 15:26:53 +0100

    Math Service third step

--- app/Modules/Math/Contracts/Math.php
+++ app/Modules/Math/Contracts/Math.php
@@ -6,7 +6,7 @@ namespace App\Modules\Math\Contracts;
 
 interface Math
 {
-    public function get(): string;
+    public function get(): array;
 
     public function check(int $answer): bool;
 }
--- app/Modules/Math/Services/MathService.php
+++ app/Modules/Math/Services/MathService.php
@@ -19,14 +19,15 @@ class MathService implements \App\Modules\Math\Contracts\Math
     /**
      * @var_1 int
      */
+    private ?int $var_1 = null;
     /**
      * @var_2 int
      */
-    private int $var_1, $var_2;
+    private ?int $var_2 = null;
     /**
      * @var string
      */
-    private string $operator;
+    private ?string $operator = null;
 
     /**
      * @param string|null $operator
@@ -35,11 +36,6 @@ class MathService implements \App\Modules\Math\Contracts\Math
     public function setOperator(string $operator = null): void
     {
         $this->operator = $operator;
-
-        if($this->operator === null) {
-            shuffle($this->operators);
-            $this->operator = $this->operators[0];
-        }
     }
 
     /**
@@ -48,7 +44,13 @@ class MathService implements \App\Modules\Math\Contracts\Math
     public function setVar1(int $var_1 = null): void
     {
         $this->var_1 = $var_1;
+    }
 
+    /**
+     * @return void
+     */
+    public function getVar1(): void
+    {
         if($this->var_1 === null) {
             $this->var_1 = mt_rand(10,999);
         }
@@ -60,12 +62,27 @@ class MathService implements \App\Modules\Math\Contracts\Math
     public function setVar2(int $var_2 = null): void
     {
         $this->var_2 = $var_2;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVar2(): void
+    {
         if($this->var_2 === null) {
             if($this->operator === '-') {
-                $this->var_2 = mt_rand($this->var_1,999);
+                $this->var_2 = mt_rand(10,$this->var_1);
             } else {
                 $this->var_2 = mt_rand(10,999);
             }
+        }
+    }
+
+    public function getOperator(): void
+    {
+        if(!$this->operator) {
+            shuffle($this->operators);
+            $this->operator = $this->operators[0];
         }
     }
 
@@ -97,11 +114,13 @@ class MathService implements \App\Modules\Math\Contracts\Math
         ];
     }
 
-    public function get(): string
+    public function get(): array
     {
+        $this->getOperator();
+        $this->getVar1();
+        $this->getVar2();
 
-
-        return '';
+        return $this->getMath($this->operator);
     }
 
     public function check(int $answer): bool
