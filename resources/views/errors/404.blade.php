5d4230908e1d22fb1e5eb74019b3b0f8c7d43f7b
helg <o.kazachkov@yandex.ru>
2024-02-15 18:21:26 +0300

    composer update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('kazachkovy.ru'))
 @section('code', 'kazachkovy.ru')
-@section('message', __('Любим вас, пурпурные наши!'))
+@section('message', __('Любим вас, пурпурные наши! <3'))
